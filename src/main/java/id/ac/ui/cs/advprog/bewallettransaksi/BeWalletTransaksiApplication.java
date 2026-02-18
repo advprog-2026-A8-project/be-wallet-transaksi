0e91034a421a19c6bf1e3cce78222125f552fcf4
Jaysen Lestari <lestarijaysen@gmail.com>
2026-02-18 21:43:16 +0700

    init springboot project

---
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/BeWalletTransaksiApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bewallettransaksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWalletTransaksiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeWalletTransaksiApplication.class, args);
+    }
+
+}
