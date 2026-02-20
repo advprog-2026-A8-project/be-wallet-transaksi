f848dc2caad4c916102a20cb5d29322d136e4ebb
Jaysen Lestari <lestarijaysen@gmail.com>
2026-02-20 20:32:36 +0700

    feat: Add wallet controller and reposioty skeleton

--- src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/repository/TransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/repository/TransactionRepository.java
@@ -1,4 +1,10 @@
 package id.ac.ui.cs.advprog.bewallettransaksi.repository;
 
-public class TransactionRepository {
+import id.ac.ui.cs.advprog.bewallettransaksi.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
 }
