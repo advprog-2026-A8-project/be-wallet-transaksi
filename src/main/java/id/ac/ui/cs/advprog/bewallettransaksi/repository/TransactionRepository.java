1945140e746914769707835e5e9441304b8722da
Jaysen Lestari <lestarijaysen@gmail.com>
2026-03-21 18:20:54 +0700

    [GREEN] Implement transaction history finder methods in TransactionRepository

--- src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/repository/TransactionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/repository/TransactionRepository.java
@@ -1,10 +1,13 @@
 package id.ac.ui.cs.advprog.bewallettransaksi.repository;
 
+import id.ac.ui.cs.advprog.bewallettransaksi.enums.TransactionStatus;
 import id.ac.ui.cs.advprog.bewallettransaksi.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
-
+    List<Transaction> findByWalletIdOrderByCreatedAtDesc(UUID walletId);
+    List<Transaction> findByWalletIdAndStatusOrderByCreatedAtDesc(UUID walletId, TransactionStatus status);
 }
