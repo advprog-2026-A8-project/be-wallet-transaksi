372ac2ca63d185b66619769dba9c4d9b415d53b2
Jaysen Lestari <lestarijaysen@gmail.com>
2026-04-14 16:39:31 +0700

    [GREEN] Enforce 2-decimal amount validation on wallet mutation DTOs

--- src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/dto/TopUpRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/dto/TopUpRequest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,5 +18,6 @@ public class TopUpRequest {
 
     @NotNull
     @DecimalMin(value = "1", message = "Amount must be at least 1")
+    @Digits(integer = 17, fraction = 2, message = "Amount must have at most 2 decimal places")
     private BigDecimal amount;
 }
--- src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/dto/WalletMutationRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/dto/WalletMutationRequest.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.bewallettransaksi.dto;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -17,6 +18,7 @@ public class WalletMutationRequest {
 
     @NotNull
     @DecimalMin(value = "1", message = "Amount must be at least 1")
+    @Digits(integer = 17, fraction = 2, message = "Amount must have at most 2 decimal places")
     private BigDecimal amount;
 
     @NotBlank
