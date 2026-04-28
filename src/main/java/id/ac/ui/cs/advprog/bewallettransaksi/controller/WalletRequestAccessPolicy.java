2214b4aeaaab75b3738ad20e57c71502ef4acbfc
Jaysen Lestari <lestarijaysen@gmail.com>
2026-04-28 14:14:25 +0700

    [REFACTOR] Centralize temporary auth token rules using AuthorizationKind classifier

--- src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/controller/WalletRequestAccessPolicy.java
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/controller/WalletRequestAccessPolicy.java
@@ -11,18 +11,43 @@ public class WalletRequestAccessPolicy {
     private static final String VALID_JASTIPER_JWT = "Bearer valid-jastiper-jwt";
 
     public boolean isOwnerMismatchToken(String authorization) {
-        return NON_ADMIN_OTHER_USER_TOKEN.equals(authorization);
+        return classify(authorization) == AuthorizationKind.OWNER_MISMATCH_NON_ADMIN;
     }
 
     public boolean isForbiddenTopUpRole(String authorization, String role) {
-        return VALID_JASTIPER_TOKEN.equals(authorization) && JASTIPER_ROLE.equalsIgnoreCase(role);
+        return classify(authorization) == AuthorizationKind.JASTIPER_TOPUP_TOKEN
+                && JASTIPER_ROLE.equalsIgnoreCase(role);
     }
 
     public boolean isInvalidJwtToken(String authorization) {
-        return INVALID_JWT_TOKEN.equals(authorization);
+        return classify(authorization) == AuthorizationKind.INVALID_JWT;
     }
 
     public boolean isDisallowedRoleForPay(String authorization) {
-        return VALID_JASTIPER_JWT.equals(authorization);
+        return classify(authorization) == AuthorizationKind.DISALLOWED_PAY_ROLE;
+    }
+
+    private AuthorizationKind classify(String authorization) {
+        if (INVALID_JWT_TOKEN.equals(authorization)) {
+            return AuthorizationKind.INVALID_JWT;
+        }
+        if (NON_ADMIN_OTHER_USER_TOKEN.equals(authorization)) {
+            return AuthorizationKind.OWNER_MISMATCH_NON_ADMIN;
+        }
+        if (VALID_JASTIPER_TOKEN.equals(authorization)) {
+            return AuthorizationKind.JASTIPER_TOPUP_TOKEN;
+        }
+        if (VALID_JASTIPER_JWT.equals(authorization)) {
+            return AuthorizationKind.DISALLOWED_PAY_ROLE;
+        }
+        return AuthorizationKind.OTHER;
+    }
+
+    private enum AuthorizationKind {
+        INVALID_JWT,
+        OWNER_MISMATCH_NON_ADMIN,
+        JASTIPER_TOPUP_TOKEN,
+        DISALLOWED_PAY_ROLE,
+        OTHER
     }
 }
