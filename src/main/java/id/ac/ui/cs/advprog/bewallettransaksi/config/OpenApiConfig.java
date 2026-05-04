89f54e61e333fae664106b9df4a916448e7c936b
Jaysen Lestari <lestarijaysen@gmail.com>
2026-05-05 01:45:56 +0700

    [GREEN] Enable OpenAPI and Swagger UI with JWT bearer scheme documentation

---
+++ src/main/java/id/ac/ui/cs/advprog/bewallettransaksi/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.bewallettransaksi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String BEARER_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI walletOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("BE Wallet Transaksi API")
+                        .version("v1")
+                        .description("Wallet and payment transaction API documentation"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME_NAME, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
+
